eb1a6327a2a224cec78c64d14c92bf60f275a309
Shashi Puttaswamy <shashi.puttaswamy@brandwatch.com>
2022-10-13 23:46:49 +0200

    mini-bcr structure addition commit

---
+++ src/main/java/com/brandwatch/minibcr/MiniBcrApplication.java
@@ -0,0 +1,13 @@
+package com.brandwatch.minibcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniBcrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brandwatch/minibcr/MiniBcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandwatch.minibcr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBcrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
