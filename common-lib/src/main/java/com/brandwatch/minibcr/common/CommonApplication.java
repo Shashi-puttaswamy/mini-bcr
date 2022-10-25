78f8d11adad1fc35ae015e837f2665d92c8e1b35
Shashi Puttaswamy <shashi.puttaswamy@brandwatch.com>
2022-10-25 23:47:27 +0200

    - sortpom addition - code style fix for structure

--- common-lib/src/main/java/com/brandwatch/minibcr/common/CommonApplication.java
+++ common-lib/src/main/java/com/brandwatch/minibcr/common/CommonApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommonApplication
-{
-    public static void main( String[] args )
-    {
+public class CommonApplication {
+    public static void main(String[] args) {
         SpringApplication.run(CommonApplication.class);
     }
 }
--- mention-generator/src/main/java/com/brandwatch/minibcr/mentiongenerator/MentionGeneratorApplication.java
+++ mention-generator/src/main/java/com/brandwatch/minibcr/mentiongenerator/MentionGeneratorApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MentionGeneratorApplication
-{
-    public static void main( String[] args )
-    {
+public class MentionGeneratorApplication {
+    public static void main(String[] args) {
         SpringApplication.run(MentionGeneratorApplication.class);
     }
 }
