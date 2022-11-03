e995bf1a31e28d4a901c4c1633acee1427fef975
Shashi Puttaswamy <shashi.puttaswamy@brandwatch.com>
2022-11-04 00:44:06 +0100

    Incorporate review comments

--- common-lib/src/main/java/com/brandwatch/minibcr/common/CommonApplication.java
+++
@@ -1,8 +0,0 @@
-package com.brandwatch.minibcr.common;
-
-public class CommonApplication {
-
-  public static void main(String[] args) {
-    System.out.println("this is common application for libs");
-  }
-}
