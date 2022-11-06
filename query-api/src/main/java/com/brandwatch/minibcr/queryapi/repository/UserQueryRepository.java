cd180b609a58251bdb33e99b9e65c8fb80356aa6
Shashi Puttaswamy <shashi.puttaswamy@brandwatch.com>
2022-11-06 17:31:35 +0100

    Redis implementation - simple redis Cacheable and CacheEvict for repo on get and save

--- query-api/src/main/java/com/brandwatch/minibcr/queryapi/repository/UserQueryRepository.java
+++ query-api/src/main/java/com/brandwatch/minibcr/queryapi/repository/UserQueryRepository.java
@@ -1,8 +1,22 @@
 package com.brandwatch.minibcr.queryapi.repository;
 
+import java.util.List;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import reactor.util.annotation.NonNull;
 
 import com.brandwatch.minibcr.queryapi.entity.UserQuery;
 
 public interface UserQueryRepository extends JpaRepository<UserQuery, Long> {
+    @Override
+    @NonNull
+    @Cacheable("user-query")
+    List<UserQuery> findAll();
+
+    @Override
+    @CacheEvict(value = "user-query", allEntries = true)
+    <S extends UserQuery> @NonNull S save(@NonNull S userQuery);
 }
