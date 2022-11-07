16a2e1422849967d0edc206616e8e3398c5d8b35
Shashi Puttaswamy <shashi.puttaswamy@brandwatch.com>
2022-11-07 23:32:44 +0100

    - Setup docker - Review comments incorporated

--- common-lib/src/main/java/com/brandwatch/minibcr/common/model/Resource.java
+++ common-lib/src/main/java/com/brandwatch/minibcr/common/model/Resource.java
@@ -1,21 +1,28 @@
 package com.brandwatch.minibcr.common.model;
 
+import java.time.Instant;
+
 public class Resource {
 
-  private final String title;
-  private final String text;
+    private final String title;
+    private final String text;
+    private final long createdTime;
 
-  public Resource(String title, String text) {
-    this.title = title;
-    this.text = text;
-  }
+    public Resource(String title, String text) {
+        this.title = title;
+        this.text = text;
+        this.createdTime = Instant.now().toEpochMilli();
+    }
 
-  public String getTitle() {
-    return title;
-  }
+    public String getTitle() {
+        return title;
+    }
 
-  public String getText() {
-    return text;
-  }
+    public String getText() {
+        return text;
+    }
 
+    public long getCreatedTime() {
+        return createdTime;
+    }
 }
